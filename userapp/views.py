12bbefe612ce81c42efff8771ec7ff5a0af2150d
liamgilbert1 <gilbert.li@northeastern.edu>
2024-08-04 18:30:20 -0400

    Select services front page

--- userapp/views.py
+++ userapp/views.py
@@ -5,4 +5,5 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'userapp/index.html')
 
-# Create your views here.
+def service(request):
+    return render(request, 'userapp/service.html')
