404155a386f271d71a9cb1b8d2bc21dc06ed58c7
Joseph-Carlucci <148010329+Joseph-Carlucci@users.noreply.github.com>
2024-08-09 22:28:17 -0400

    Javascript & css clean up

--- userapp/static/userapp/js/service.js
+++ userapp/static/userapp/js/service.js
@@ -1,146 +1,17 @@
+// the active page of the form
 let activePage;
+
+// the pages of the form
 let pages;
-let nextButtons;
-let backButtons;
+
+// the chechpoints of the progress bar
 let progressCheckpoints;
+
+// the lines of the progress bar connecting the checkpoints
 let progressLines;
+
+// the text which shows the progress of the user under the progress bar checkpoints
 let progressText;
-let services;
-
-document.addEventListener("DOMContentLoaded", function() {
-
-    activePage = document.getElementById("service-selector");
-    
-    nextButtons = document.querySelectorAll('.next');
-    nextButtons.forEach(button => {
-        button.addEventListener("click", next);
-    });
-
-    backButtons = document.querySelectorAll('.back');
-    backButtons.forEach(button => {
-        button.addEventListener("click", back);
-    });
-
-    services = document.querySelectorAll('.service');
-    services.forEach(service => {
-        service.addEventListener("click", function() {
-            select(service)
-        }, {once: true});
-    });
-
-    progressLines = document.querySelectorAll('.progress-bar-line');
-    
-    progressText = document.querySelectorAll('.progress-text');
-    progressText[0].classList.add("active-progress-text");
-    progressText[0].classList.add("activated-progress-text");
-
-    progressCheckpoints = document.querySelectorAll('.checkpoint');
-    progressCheckpoints[0].classList.add("active-progress");
-    progressCheckpoints = Array.from(progressCheckpoints).slice(1);
-
-    pages = [document.getElementById("service-selector"), document.getElementById("service-details"), document.getElementById("estimates"), document.getElementById("submit")];
-    console.log("Services:", services);
-});
-
-function next() {
-    if (activePage === pages[pages.length - 1]) {
-        return;
-    }
-
-    let currentPageIndex = pages.indexOf(activePage);
-    let newActivePage = pages[currentPageIndex + 1];
-
-    progressLines[currentPageIndex].classList.add("active-progress");
-    progressCheckpoints[currentPageIndex].classList.add("active-progress");
-    
-    slideLeft(activePage);
-    slideIn(newActivePage);
-    
-    activePage.addEventListener("transitionend", function handleTransition() {
-        activePage.style.display = "none";
-        activePage.removeEventListener("transitionend", handleTransition);
-        activePage = newActivePage;
-        progressText[currentPageIndex + 1].classList.add("activated-progress-text");
-        progressText[currentPageIndex + 1].classList.add("active-progress-text");
-        progressText[currentPageIndex].classList.remove("active-progress-text");
-        // check if page is the service details page and if it is, call the initAutocomplete function
-        if (activePage === pages[1]) {
-            initAutocomplete();
-        }
-    });
-}
-
-function back() {
-    if (activePage === pages[0]) {
-        return;
-    }
-
-    let currentPageIndex = pages.indexOf(activePage);
-    let newActivePage = pages[currentPageIndex - 1];
-
-    progressLines[currentPageIndex - 1].classList.remove("active-progress");
-    progressText[currentPageIndex].classList.remove("activated-progress-text");
-    progressText[currentPageIndex].classList.remove("active-progress-text");
-    progressCheckpoints[currentPageIndex - 1].classList.remove("active-progress");
-    progressText[currentPageIndex - 1].classList.add("active-progress-text");
-    
-    slideRight(activePage);
-    slideIn(newActivePage);
-    
-    activePage.addEventListener("transitionend", function handleTransition() {
-        activePage.style.display = "none";
-        activePage.removeEventListener("transitionend", handleTransition);
-        activePage = newActivePage;
-    });
-}
-
-function slideLeft(page) {
-    requestAnimationFrame(() => {
-        page.style.transform = "translateX(-100vw)";
-    });
-}
-
-function slideIn(page) {
-    page.style.display = "flex";
-    requestAnimationFrame(() => {
-        page.style.transform = "translateX(0)";
-    });
-}
-
-function slideRight(page) {
-    requestAnimationFrame(() => {
-        page.style.transform = "translateX(100vw)";
-    });
-}
-
-
-function select(service) {
-    if (service.classList.contains("selected-service")) {
-        return;
-    }
-    let newService = service.cloneNode(true);
-    newService.classList.add("added-service");
-    service.classList.add("selected-service");
-    document.getElementById("selected-services").appendChild(newService);
-    service.addEventListener("click", function() {
-        deselect(service);
-        document.getElementById("selected-services").removeChild(newService);
-    }, {once: true});
-}
-
-function deselect(service) {
-    if (!service.classList.contains("selected-service")) {
-        return;
-    }
-    service.classList.remove("selected-service");
-    service.addEventListener("click", function() {
-        select(service);
-    }, {once: true});
-}
-
-
-
-// Description: This file contains the javascript code for the index.html file.
 
 // determines if the search box has been filled out with an address selected from the autocomplete dropdown
 let searchable = false;
@@ -172,8 +43,144 @@ let polygons = [];
 // the number of polygons, used to assign an id to each polygon
 let numPolygons = 0;
 
-// first function to be called when the page is loaded
-// receives the callback from the google maps api
+// event listener for the DOMContentLoaded event
+// initializes the variables and adds event listeners
+document.addEventListener("DOMContentLoaded", function() {
+    activePage = document.getElementById("service-selector");
+    
+    let nextButtons = document.querySelectorAll('.next');
+    nextButtons.forEach(button => {
+        button.addEventListener("click", next);
+    });
+
+    let backButtons = document.querySelectorAll('.back');
+    backButtons.forEach(button => {
+        button.addEventListener("click", back);
+    });
+
+    let services = document.querySelectorAll('.service');
+    services.forEach(service => {
+        service.addEventListener("click", function() {
+            select(service)
+        }, {once: true});
+    });
+
+    progressLines = document.querySelectorAll('.progress-bar-line');
+    
+    progressText = document.querySelectorAll('.progress-text');
+    progressText[0].classList.add("active-progress-text");
+    progressText[0].classList.add("activated-progress-text");
+
+    progressCheckpoints = document.querySelectorAll('.checkpoint');
+    progressCheckpoints[0].classList.add("active-progress");
+    progressCheckpoints = Array.from(progressCheckpoints).slice(1);
+
+    pages = [document.getElementById("service-selector"), document.getElementById("service-details"), document.getElementById("estimates"), document.getElementById("submit")];
+    console.log("Services:", services);
+});
+
+// function to go to the next page
+function next() {
+    if (activePage === pages[pages.length - 1]) {
+        return;
+    }
+
+    let currentPageIndex = pages.indexOf(activePage);
+    let newActivePage = pages[currentPageIndex + 1];
+
+    progressLines[currentPageIndex].classList.add("active-progress");
+    progressCheckpoints[currentPageIndex].classList.add("active-progress");
+    
+    slideLeft(activePage);
+    slideIn(newActivePage);
+    
+    activePage.addEventListener("transitionend", function handleTransition() {
+        activePage.style.display = "none";
+        activePage.removeEventListener("transitionend", handleTransition);
+        activePage = newActivePage;
+        progressText[currentPageIndex + 1].classList.add("activated-progress-text");
+        progressText[currentPageIndex + 1].classList.add("active-progress-text");
+        progressText[currentPageIndex].classList.remove("active-progress-text");
+        // check if page is the service details page and if it is, call the initAutocomplete function
+        if (activePage === pages[1]) {
+            initAutocomplete();
+        }
+    });
+}
+
+// function to go back to the previous page
+function back() {
+    if (activePage === pages[0]) {
+        return;
+    }
+
+    let currentPageIndex = pages.indexOf(activePage);
+    let newActivePage = pages[currentPageIndex - 1];
+
+    progressLines[currentPageIndex - 1].classList.remove("active-progress");
+    progressText[currentPageIndex].classList.remove("activated-progress-text");
+    progressText[currentPageIndex].classList.remove("active-progress-text");
+    progressCheckpoints[currentPageIndex - 1].classList.remove("active-progress");
+    progressText[currentPageIndex - 1].classList.add("active-progress-text");
+    
+    slideRight(activePage);
+    slideIn(newActivePage);
+    
+    activePage.addEventListener("transitionend", function handleTransition() {
+        activePage.style.display = "none";
+        activePage.removeEventListener("transitionend", handleTransition);
+        activePage = newActivePage;
+    });
+}
+
+// function to slide the page to the left
+function slideLeft(page) {
+    requestAnimationFrame(() => {
+        page.style.transform = "translateX(-100vw)";
+    });
+}
+
+// function to slide the page to the center
+function slideIn(page) {
+    page.style.display = "flex";
+    requestAnimationFrame(() => {
+        page.style.transform = "translateX(0)";
+    });
+}
+
+// function to slide the page to the right
+function slideRight(page) {
+    requestAnimationFrame(() => {
+        page.style.transform = "translateX(100vw)";
+    });
+}
+
+// selects a service and adds it to the selected services list
+function select(service) {
+    if (service.classList.contains("selected-service")) {
+        return;
+    }
+    let newService = service.cloneNode(true);
+    newService.classList.add("added-service");
+    service.classList.add("selected-service");
+    document.getElementById("selected-services").appendChild(newService);
+    service.addEventListener("click", function() {
+        deselect(service);
+        document.getElementById("selected-services").removeChild(newService);
+    }, {once: true});
+}
+
+// deselects a service and removes it from the selected services list
+function deselect(service) {
+    if (!service.classList.contains("selected-service")) {
+        return;
+    }
+    service.classList.remove("selected-service");
+    service.addEventListener("click", function() {
+        select(service);
+    }, {once: true});
+}
+
 // initializes the autocomplete object and adds event listeners
 function initAutocomplete() {
   // create the autocomplete object
