1698be9c46f7b0020dfa69816949477c1bf3b825
Joseph-Carlucci <148010329+Joseph-Carlucci@users.noreply.github.com>
2024-08-21 21:16:16 -0400

    Home Page Init

--- accelerate/urls.py
+++ accelerate/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('userapp/', include('userapp.urls')),
+    path('home/', include('home.urls')),
 ]
