fb5eaa2fa99885def10dca9606679035ca63acc5
Joseph-Carlucci <148010329+Joseph-Carlucci@users.noreply.github.com>
2024-09-18 03:12:55 -0400

    Initial DB Commit

---
+++ core/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+
+class LandscapingCompany(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    pricing_model = models.ForeignKey('PricingModel', on_delete=models.PROTECT)
+
+
+    def __str__(self):
+        return self.name
+    
+
+class PricingModel(models.Model):
+    pricing_model_name = models.CharField(max_length=100)
+    per_square_foot = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.pricing_model_name
--- userapp/urls.py
+++ userapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("service/", views.service, name="service"),
+    path("calculate-quote/", views.calculate_quote, name="calculate_quote"),
 ]
